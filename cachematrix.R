99e5e9c4b7ca9b15d81ce2943c4492a01f814332
Mr. Paul Blart <mbailey@schneiderdcim.com>
2015-11-05 19:12:42 -0500

    Created the two methods outlined in the assignment.
    1) Create a matrix object capable of caching the results of it's inverse
    2) A function that exercises this capabaility by printing when the cache'd version is used

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## This is an extension of the built in matrix type that contains additional
+## functionality to get/set the inverse of itself (cache)
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  setInverse <- function(solve) m <<- solve
+  getInverse <- function() m
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## This method attempts to get the cached inverse of the supplied
+## matrix.  If none exists then it is computed and stored in the
+## object's cache
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setInverse(m)
+  m
 }
